469cafaed9cb5fb719303365f2036e197abe473a
Ulrik Einarson <torvalde@gmail.com>
2020-04-16 23:34:10 +0200

    fixed outdated test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const  buttonElement = getByText(/Hello/i);
+  expect(buttonElement).toBeInTheDocument();
 });
